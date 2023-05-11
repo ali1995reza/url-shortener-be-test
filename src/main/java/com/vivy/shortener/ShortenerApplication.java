dccd526d6b5df09f165e23c27d3465d97a3a12b3
Alireza Akhoundi <alirezaakhoundi73@gmail.com>
2023-05-12 01:22:47 +0330

    fix project structure, update gradle version to support java version 17 and spring-boot 3.0.6 , add necessary libraries

--- src/main/java/com/vivy/shortener/ShortenerApplication.java
+++ src/main/java/com/vivy/shortener/ShortenerApplication.java
@@ -1,13 +1,13 @@
-package com.github.vivyteam;
+package com.vivy.shortener;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Application {
+public class ShortenerApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(Application.class, args);
+        SpringApplication.run(ShortenerApplication.class, args);
     }
 
 }
